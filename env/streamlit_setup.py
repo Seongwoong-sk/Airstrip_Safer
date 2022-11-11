d0c370dc2ae692e68e3e1cd8406a18bc42055751
Seong Woong Kim <rlatjddnd3@gmail.com>
2022-11-11 23:00:26 +0900

    Colab 내 Streamlit을 위한 라이브러리, 패키지 설치

--- env/streamlit_setup.py
+++ env/streamlit_setup.py
@@ -1,6 +1,6 @@
 import os
 
-# Install Pytorch+cuda
+# Install Pytorch+cuda 
 os.system('pip install -q torch==1.11.0+cu113 torchvision==0.12.0+cu113 torchaudio==0.11.0 --extra-index-url https://download.pytorch.org/whl/cu113')
 import torch
 print(torch.__version__)
